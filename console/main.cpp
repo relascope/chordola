f8993bce3aad1982291bd304ac0e1362cbbf002d
relascope <guru@dojoy.at>
2023-08-19 16:17:19 +0200

    print only new chords

--- console/main.cpp
+++ console/main.cpp
@@ -2,13 +2,22 @@
 
 #include "VERSION.h"
 #include "audiobackend.h"
+#include "chord.h"
+
+void ChordArrived(Chord newChord) {
+  static Chord oldChord{0, 0, 0};
+  if (oldChord != newChord) {
+    oldChord = newChord;
+    std::cout << newChord;
+  }
+}
 
 int main(int argc, char *argv[]) {
   std::cout << "chord-rec console: Realtime Chord Recognition\nVersion: "
             << CHORD_REC_CONSOLE_VERSION << '\n';
 
-    registerChordListener([] (Chord c) { std::cout << c; });
-    connectAudioBackend(argv[0]);
+  registerChordListener(&ChordArrived);
+  connectAudioBackend(argv[0]);
 
   return 0;
 }
