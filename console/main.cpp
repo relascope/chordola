92ef292ea2c10e89d512e608c719a2b7e5955bb6
relascope <guru@dojoy.at>
2023-08-19 21:36:09 +0200

    separate thread for output to isolate DSP-CPU-load

--- console/main.cpp
+++ console/main.cpp
@@ -1,20 +1,33 @@
+#include <atomic>
+#include <chrono>
 #include <iostream>
+#include <thread>
 
 #include "VERSION.h"
 #include "audiobackend.h"
 #include "chord.h"
 
-void ChordArrived(Chord newChord) {
-  static Chord oldChord{0, 0, 0};
-  if (oldChord != newChord) {
-    oldChord = newChord;
-    std::cout << newChord;
+static std::atomic<Chord> actChord;
+
+void consumeThread() {
+  while (true) {
+    static Chord oldChord{0, 0, 0};
+    if (oldChord != actChord) {
+      oldChord = actChord;
+      std::cout << actChord;
+    }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(2));
   }
 }
+
+void ChordArrived(Chord newChord) { actChord = newChord; }
 
 int main(int argc, char *argv[]) {
   std::cout << "chord-rec console: Realtime Chord Recognition\nVersion: "
             << CHORD_REC_CONSOLE_VERSION << '\n';
+
+  std::thread consume(consumeThread);
 
   registerChordListener(&ChordArrived);
   connectAudioBackend(argv[0]);
